082d644df4bd2902053d5182f4dc784f0f3e78e6
Aadil Hamza <85585327+aadilhamza@users.noreply.github.com>
2024-01-02 02:01:26 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Muhammad Adil Hamza",
+  title: "Machine Learning Engineer",
+  email: "muhammadadilhamza@acm.org",
+  gitHub: "aadil-hamza",
+  instagram: "@aadilhamza.ai",
+  linkedIn: "Aadil Hamza",
+  medium: "adilhamza782",
+  twitter: "theaadilhamza",
+  youTube: "Aadil Hamza",
 };
 
 const primaryColor = "#4E567E";
